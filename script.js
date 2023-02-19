f78be31f210a74cfe562717710d9c36ac6ebdef7
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-02-19 17:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+function generateRandomNumber() {
+	var randomNumber = Math.floor(Math.random()*20)+(-20);
+	document.getElementById("num").textContent = randomNumber;
+	var inputValue = document.getElementById("guess").value;
+	var diff = Math.abs(Number(inputValue)-randomNumber);
+	if(diff<=5){
+		document.getElementById("respond").textContent = "Hot";
+	}
+	else{
+		document.getElementById("respond").textContent = "Cold";
+	}
+}
 
+document.getElementById("btn").addEventListener("click",generateRandomNumber);
